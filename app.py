ef6f0f834c705b990c1023d02d4200d43f1f8224
egonzalezqw <egofac@gmail.com>
2026-03-19 10:17:20 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("ROI Calculator - Ansible")
+
+servidores = st.number_input("Número de servidores", 50)
+admins = st.number_input("Administradores", 3)
+salario = st.number_input("Salario mensual", 2500)
+reduccion = st.slider("Reducción (%)", 0, 100, 70)
+
+costo_anual = admins * salario * 12
+ahorro = costo_anual * (reduccion/100)
+
+st.metric("Costo anual", costo_anual)
+st.metric("Ahorro estimado", ahorro)
