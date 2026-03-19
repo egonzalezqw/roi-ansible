54db030d8330d265a1ceaaafa15b84e8c5f7d579
egonzalezqw <egofac@gmail.com>
2026-03-19 10:24:50 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("ROI Calculator - Ansible")
 
 servidores = st.number_input("Número de servidores", 1)
-admins = st.number_input("Administradores", 3)
+admins = st.number_input("Administradores", 1)
 salario = st.number_input("Salario mensual", 500)
 reduccion = st.slider("Reducción (%)", 0, 100, 70)
 
